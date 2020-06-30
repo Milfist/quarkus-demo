cd01cb9f7d61b555f23b59a941a4db5fe2c89e71
“Milfist” <milfist@gmail.com>
2020-06-30 17:56:06 +0200

    first commit

---
+++ users/src/main/java/com/milfist/Users.java
@@ -0,0 +1,16 @@
+package com.milfist;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+public class Users {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
